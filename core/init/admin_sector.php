0e5e6a4e8fdaad247a1a2d53ba488f466a38eb85
MicronEngine <zmigpost@gmail.com>
2014-03-27 20:30:46 +0300

    Update admin_sector.php

--- core/init/admin_sector.php
+++ core/init/admin_sector.php
@@ -2,13 +2,17 @@
 
     require_once BASEPATH . 'core/init/db.php';
 
+    // Изначальная папка нам может понадобится для CKEditor
+    $g_config['extrapacker']['non_admin_dir'] = $g_config['extrapacker']['dir'];
+
     // Проверяем вход в админку и авторизацию
     $isAdminSector = (stripos(strtolower(GetQuery()), 'admin/') === 0 || GetQuery() === 'admin');
     if ($isAdminSector)
     {
         // Меняем папку куда будут складироваться css/js админки
-        $g_config['extrapacker']['dir'] = 'extrapacker_admin';
-        $g_config['mainTpl']            = 'admin/main_tpl';
+        $g_config['extrapacker']['dir']     = 'extrapacker_admin';
+        $g_config['extrapacker']['packCss'] = true;
+        $g_config['mainTpl']                = 'admin/main_tpl';
 
         $g_adminAuth = new AdminModel();
         $g_adminAuth->ChkLogin();
