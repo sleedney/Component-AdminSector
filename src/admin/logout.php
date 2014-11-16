aa91e0b45779ed1a3bd5858ac8049aba01b9716e
MicronEngine <zmigpost@gmail.com>
2014-11-16 04:26:33 +0200

    Update logout.php

--- src/admin/logout.php
+++ src/admin/logout.php
@@ -1,7 +1,7 @@
 <?php
 
     $admin = new AdminModel(NULL, true);
-    $admin->Logout();
+    $admin->DoLogout();
 
     header("Location: " . SiteRoot($g_config['admin_sector']['after_logout_page']));
     exit();
